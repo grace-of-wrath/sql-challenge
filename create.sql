f648428d6fd9eedc7dce39d427c65860b2395ac3
grace-of-wrath <135581863+grace-of-wrath@users.noreply.github.com>
2023-08-16 21:11:55 -0700

    made create statements, loaded data, and started queries

--- create.sql
+++ create.sql
@@ -1,30 +1,36 @@
 --Create statements
 
-CREATE TABLE departments
-dept_no VARCHAR PRIMARY KEY
-dept_name VARCHAR
+CREATE TABLE departments (
+	dept_no VARCHAR PRIMARY KEY,
+	dept_name VARCHAR);
 
-CREATE TABLE dept_emp
-emp_no VARCHAR
-FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+CREATE TABLE titles (
+	title_id VARCHAR PRIMARY KEY,
+	title VARCHAR);
 
-CREATE TABLE dept_manager
-dept_no 
-emp_no
+CREATE TABLE employees (
+	emp_no INTEGER PRIMARY KEY,
+	emp_title_id VARCHAR,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
+	birth_date VARCHAR,
+	first_name VARCHAR,
+	last_name VARCHAR,
+	sex VARCHAR,
+	hire_date VARCHAR);
 
-CREATE TABLE employees
-emp_no
-emp_title_id
-birth_date
-first_name
-last_name
-sex
-hire_date
+CREATE TABLE dept_emp (
+	emp_no INTEGER,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	dept_no VARCHAR,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no));
 
-CREATE TABLE salaries
-emp_no 
-salary
+CREATE TABLE dept_manager (
+	dept_no VARCHAR,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	emp_no INTEGER,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no));
 
-CREATE TABLE titles
-title_id
-title
+CREATE TABLE salaries (
+	emp_no INTEGER PRIMARY KEY,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	salary INTEGER);
